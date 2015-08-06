85086fbd719a231118829fd39ac61ff317d4e2a8
Elizabeth Chamberlain <elizzybeth@gmail.com>
2015-08-05 23:16:29 -0400

    Gets indices. Gets articles. Fails to save article data.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var setupCrawler = function(collection){
         
         // Populate this with UofL cookie
         headers: {
-            'Cookie': 'optimizelyEndUserId=oeu1397855711890r0.06903332588262856; is_returning=1; oup-cookie=1_29-10-2014; wt3_eid=%3B935649882378213%7C2143446019600667503; AMCV_4D6368F454EC41940A4C98A6%40AdobeOrg=1256414278%7CMCMID%7C29197735351980510733684863018516445160%7CMCAAMLH-1436400486%7C7%7CMCAAMB-1436400486%7CNRX38WO0n5BH8Th-nqAG_A%7CMCAID%7CNONE; optimizelySegments=%7B%22204658328%22%3A%22false%22%2C%22204728159%22%3A%22none%22%2C%22204736122%22%3A%22referral%22%2C%22204775011%22%3A%22gc%22%2C%22700471485%22%3A%22true%22%7D; optimizelyBuckets=%7B%7D; s_lv=1435795835454; __utma=90476039.1627630122.1392921203.1435878556.1437956432.3; __utmz=90476039.1434375083.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); AMCV_774C31DD5342CAF40A490D44%40AdobeOrg=1256414278%7CMCMID%7C29186752574512628033686031665310678970%7CMCAAMLH-1438783857%7C7%7CMCAAMB-1438783857%7CNRX38WO0n5BH8Th-nqAG_A%7CMCAID%7C2A804F518507AECF-6000010A20036FBD; s_fid=2E6A19D1E95E1B6A-055D4F9EFBCAC671; mbox=PC#1438178959041-184916.28_59#1439817486|session#1438607885632-91475#1438609746; _ga=GA1.2.1627630122.1392921203; PQIL=1246; JSESSIONID=A80C84B95E40D8EE68EAC382DA76E552; UID="UNIVERSITY OF LOUISVILLE-KY"; AWSELB=8F41BF69186E7A7E737F7DCEB711ECC28A2C51FB40DFA71E752810FB4E3AD1719ACE39F600987B5BA14F169806D8B425FBF56C6575F29BC9865BF80A0F331AC7E1D3B24EDE; ezproxy=F4ZxDcIu7fpmXLM'
+            'Cookie': 'optimizelyEndUserId=oeu1397855711890r0.06903332588262856; is_returning=1; oup-cookie=1_29-10-2014; wt3_eid=%3B935649882378213%7C2143446019600667503; AMCV_4D6368F454EC41940A4C98A6%40AdobeOrg=1256414278%7CMCMID%7C29197735351980510733684863018516445160%7CMCAAMLH-1436400486%7C7%7CMCAAMB-1436400486%7CNRX38WO0n5BH8Th-nqAG_A%7CMCAID%7CNONE; optimizelySegments=%7B%22204658328%22%3A%22false%22%2C%22204728159%22%3A%22none%22%2C%22204736122%22%3A%22referral%22%2C%22204775011%22%3A%22gc%22%2C%22700471485%22%3A%22true%22%7D; optimizelyBuckets=%7B%7D; s_lv=1435795835454; __utma=90476039.1627630122.1392921203.1435878556.1437956432.3; __utmz=90476039.1434375083.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); AMCV_774C31DD5342CAF40A490D44%40AdobeOrg=1256414278%7CMCMID%7C29186752574512628033686031665310678970%7CMCAAMLH-1438783857%7C7%7CMCAAMB-1438783857%7CNRX38WO0n5BH8Th-nqAG_A%7CMCAID%7C2A804F518507AECF-6000010A20036FBD; s_fid=2E6A19D1E95E1B6A-055D4F9EFBCAC671; mbox=PC#1438178959041-184916.28_59#1439817486|session#1438607885632-91475#1438609746; _ga=GA1.2.1627630122.1392921203; PQIL=1246; AWSELB=8F41BF69186E7A7E737F7DCEB711ECC28A2C51FB40DFA71E752810FB4E3AD1719ACE39F600987B5BA14F169806D8B425FBF56C6575F29BC9865BF80A0F331AC7E1D3B24EDE; ezproxy=ny7QNAP5CnMGr9V; JSESSIONID=E39AEAFBC9585E78ECBC77B01F4BFE2A; UID="UNIVERSITY OF LOUISVILLE-KY"'
         },
         
         // Called for each crawled page 
@@ -34,7 +34,6 @@ var setupCrawler = function(collection){
                 $("a[href*='searchFullText']").each(function(index,a) {
                     console.log("Checking " + a.href);
                     collection.findOne({URL: a.href}, function(err, article){
-                        console.log("The article is: " + article);
                         if(err){
                             console.log("Article exist check failed.");
                             console.log(err);
@@ -50,7 +49,9 @@ var setupCrawler = function(collection){
                 });
                 if(indexOriginal === result.request.uri.href) {
                     // on the original index page
+                    console.log("On the original index page.");
                     // so queue all the other index pages
+                    console.log("So queueing all the other index pages.");
                     $(".unstyled .alphalinks > a").each(function() {
                         var currentTOC = $(this).attr("href");
                         console.log("Current TOC: ", currentTOC);
@@ -58,7 +59,7 @@ var setupCrawler = function(collection){
                     });
                 }
             } else {
-                console.log("Reached the else!");
+                console.log("Reached the else! Saving article data.");
                 // it's an article
                 // so save the article data
                 
